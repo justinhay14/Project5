482cf4654e87725a1bf353334ae7597e7b1caf14
justinhay14 <54778974+justinhay14@users.noreply.github.com>
2019-11-29 13:44:32 -0500

    some implementation

--- ReservationClient.java
+++ ReservationClient.java
@@ -1,2 +1,40 @@
+import javax.swing.*;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class ReservationClient {
+    public static void main(String args[]) {
+        Socket socket;
+        try {
+            String ip = JOptionPane.showInputDialog(null,
+                    "Type in the hostname of the Reservation Server.", "Airport Manager",
+                    JOptionPane.QUESTION_MESSAGE);
+            if(ip == null) {
+                System.exit(0);
+            }
+            String port1 = JOptionPane.showInputDialog(null,
+                    "Type in the port of the Reservation Server.", "Airport Manager",
+                    JOptionPane.QUESTION_MESSAGE);
+            if(port1 == null) {
+                System.exit(0);
+            }
+            int port = Integer.parseInt(port1);
+            socket = new Socket(ip, port);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Invalid hostname and port.",
+                    "Airport Manager", JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
+        ImageIcon icon = new ImageIcon("src/image.png");
+        JOptionPane.showMessageDialog(null, "Welcome", "Airport Manager",
+                JOptionPane.INFORMATION_MESSAGE, icon);
+        JFrame frame = new JFrame("Airport Manager");
+        frame.setSize(1280,720);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        JPanel panel = new JPanel();
+        JComboBox<String> airline = new JComboBox<>();
+        panel.add(airline);
+        frame.setVisible(true);
+    }
 }
--- ReservationServer.java
+++ ReservationServer.java
@@ -1,2 +1,16 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.util.*;
 public class ReservationServer {
+    public static void main(String args[]) {
+        /*Random r = new Random();
+        int port = r.nextInt(65536);
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Hosting Reservation Server on port: " + port);
+            serverSocket.accept();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+    }
 }
