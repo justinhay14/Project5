e42fd97b13a6650484b0fa2b73692ff6826ad4e3
justinhay14 <54778974+justinhay14@users.noreply.github.com>
2019-11-29 21:11:36 -0500

    some implementation

--- ReservationClient.java
+++ ReservationClient.java
@@ -1,11 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class ReservationClient {
     public static void main(String args[]) {
-        Socket socket;
+        /*Socket socket;
         try {
             String ip = JOptionPane.showInputDialog(null,
                     "Type in the hostname of the Reservation Server.", "Airport Manager",
@@ -25,16 +26,42 @@ public class ReservationClient {
             JOptionPane.showMessageDialog(null, "Invalid hostname and port.",
                     "Airport Manager", JOptionPane.ERROR_MESSAGE);
             System.exit(0);
-        }
+        }*/
         ImageIcon icon = new ImageIcon("src/image.png");
         JOptionPane.showMessageDialog(null, "Welcome", "Airport Manager",
                 JOptionPane.INFORMATION_MESSAGE, icon);
         JFrame frame = new JFrame("Airport Manager");
-        frame.setSize(1280,720);
+        frame.setLayout(new BorderLayout());
+        frame.setSize(350,300);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         JPanel panel = new JPanel();
+        JLabel label = new JLabel("Choose a flight from the drop down menu.");
         JComboBox<String> airline = new JComboBox<>();
+        JTextArea description = new JTextArea();
+        airline.addItem("Alaska");
+        airline.addItem("Delta");
+        airline.addItem("Southwest");
+        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JButton exit = new JButton("EXIT");
+        JButton chooseFlight = new JButton("CHOOSE THIS FLIGHT");
+        panel.add(label);
         panel.add(airline);
+        panel.add(description);
+        bottomPanel.add(exit);
+        bottomPanel.add(chooseFlight);
+        frame.add(panel);
+        frame.add(bottomPanel, BorderLayout.SOUTH);
         frame.setVisible(true);
+        airline.setSelectedIndex(-1);
+        while(true) {
+            int selection = airline.getSelectedIndex();
+            if(selection == 0) {
+                description.setText("Alaska----------------------------------------------------------------------------------------------------------------------------------");
+            } else if(selection == 1) {
+                description.setText("Delta");
+            } else if(selection == 2) {
+                description.setText("Southwest");
+            }
+        }
     }
 }
