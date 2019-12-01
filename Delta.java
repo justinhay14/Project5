07738aad31401f0096059887041f5ad027fee9c3
jmich867 <54598080+jmich867@users.noreply.github.com>
2019-11-30 21:15:41 -0600

    Updated Delta.java
    initialized passengers arraylist in constructor, incremented spotsFilled in addPassenger method

--- Delta.java
+++ Delta.java
@@ -4,7 +4,9 @@ public class Delta implements Airline {
     private int capacity;
     private int spotsFilled;
     private ArrayList<Passenger> passengers;
+
     public Delta(int capacity, int spotsFilled) {
+        passengers = new ArrayList<>();
         this.capacity = capacity;
         this.spotsFilled = spotsFilled;
     }
@@ -30,6 +32,7 @@ public class Delta implements Airline {
 
     @Override
     public void addPassenger(Passenger passenger) {
+        spotsFilled++;
         passengers.add(passenger);
     }
     public String toString() {
